0fe16905f549cbb1ddf26f66c8dd68f5bbd6ccc6
zutheme <hatazu@gmail.com>
2020-11-10 14:58:44 +0700

    component

--- resources/views/teamilk/master.blade.php
+++ resources/views/teamilk/master.blade.php
@@ -66,7 +66,7 @@
 
   <link href="{{ asset('assets-tea/assets/demos/default/css/plugins.css') }}" rel="stylesheet" type="text/css">
 
-  <link href="{{ asset('assets-tea/assets/demos/default/css/components.css?v=0.1.0') }}" id="style_components" rel="stylesheet" type="text/css">
+  <link href="{{ asset('assets-tea/assets/demos/default/css/components.css?v=0.1.1') }}" id="style_components" rel="stylesheet" type="text/css">
 
   <link href="{{ asset('assets-tea/assets/demos/default/css/themes/default.css') }}" rel="stylesheet" id="style_theme" type="text/css">
 
