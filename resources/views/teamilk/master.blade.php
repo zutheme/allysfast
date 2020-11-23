1872f5fa6f34515c351703412ddc08d6062b5bfe
hatazu <hatazu@gmail.com>
2020-11-23 14:14:45 +0700

    pixel

--- resources/views/teamilk/master.blade.php
+++ resources/views/teamilk/master.blade.php
@@ -327,6 +327,23 @@
       src="https://www.facebook.com/tr?id=978611442541383&ev=PageView&noscript=1"
       /></noscript>
       <!-- End Facebook Pixel Code -->
+      <!-- Facebook Pixel Code -->
+      <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '744337253095762');
+      fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=744337253095762&ev=PageView&noscript=1"
+      /></noscript>
+      <!-- End Facebook Pixel Code -->
       <!-- Global site tag (gtag.js) - Google Analytics -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-161438149-1">
 		</script>
